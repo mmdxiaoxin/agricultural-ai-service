8ae712cc3262a27830791ddf6e2ff2efb7c0a407
小新 <782446723@qq.com>
2025-03-10 14:22:43 +0800

    feat: 简单实现yolo8目标检测功能

--- common/enum/weight.py
+++ common/enum/weight.py
@@ -5,7 +5,9 @@ from enum import Enum
 
 
 class ModelWeights(Enum):
-    YOLO8_DETECT = None
+    YOLO8_DETECT = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "../weight/yolo8/detect-best.pt")
+    )
     YOLO5_DETECT = None
     YOLO11_CLASSIFY = os.path.abspath(
         os.path.join(os.path.dirname(__file__), "../weight/yolo11/classify-best.pt")
--- modules/yolo8/service.py
+++ modules/yolo8/service.py
@@ -1,8 +1,13 @@
+from common.models import YOLOv8Detector
+
+model = YOLOv8Detector()
+
+
 def plant_detect_service(image_data):
     """
     使用 YOLO 对图像进行推理，返回分类结果的序列化数据。
-
     :param image_data: 输入的图片数据，类型为 bytes
     :return: 序列化的推理结果
     """
-    pass
+    results = model.detect(image_data)
+    return results
